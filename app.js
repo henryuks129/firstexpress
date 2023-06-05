12a45b11321f75dfc0466dc3c83e5a9565d236de
HENRY <henryuks129@gmail.com>
2023-06-05 13:49:00 +0100

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 8080
 
 // config ejs
 app.set('view engine', 'ejs')
+require('dotenv').config()
 
-// Routes
+
+// custom middleware
+// app.get((req,res,next)=>{
+    //     console.log('a request was just made');
+    //     console.log(req.method);
+    //     console.log(req.path);
+    //     next()
+    // })
+app.use(express.static('public'))
+mongoose.connect(process.env.DBURL)
+.then(()=> console.log('DB connected successfully'))
+.catch((err)=>console.log(err))
+    
+ // Routes
 const trainees = [
     {name: 'Christy',profession: 'front-end dev'},
     {name: 'Ejiro',profession: 'back-end dev'},
@@ -14,12 +29,17 @@ const trainees = [
     {name: 'John',profession: 'desktop dev'},
 ]
 
+
 app.get('/',(req,res)=>{
     res.render('index',{title: 'EJS Home Page',trainees})
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{title: 'EJS About Page',trainees})
+})
+
+app.get('/todo/create',(req,res)=>{
+    res.render('createList',{title: 'EJS createList page'})
 })
 
 app.use((req,res)=>{
